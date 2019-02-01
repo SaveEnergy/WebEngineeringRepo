67a07f37aaa4eb5dab827f31c3e63999a2b3df19
Sinan Ö <46645793+SaveEnergy@users.noreply.github.com>
2019-02-01 09:54:22 +0100

    Just playing around

---
+++ JavaScriptTest/HelloWorld.js
@@ -0,0 +1,31 @@
+function onMouseOver(id) {
+
+    string = document.getElementById(id).innerText;
+
+    if ( string != 'Changed') {
+        document.getElementById(id).addEventListener('mouseover', function () {
+            document.getElementById(id).textContent = 'Changed';
+        });
+    }
+
+    document.getElementById(id).addEventListener('mouseout', function () {
+        document.getElementById(id).textContent = 'JS Testseite';
+    });
+
+}
+
+function init() {
+
+    console.log('DOM loaded!');
+
+    document.getElementById('source').innerHTML = '<div ' +
+        ' id = "content"></div>';
+    document.getElementById('content').innerHTML = '<h1 ' +
+        ' id="headline">JS Testseite</h1>';
+
+    onMouseOver('headline');
+
+
+}
+
+document.addEventListener('DOMContentLoaded', init);
