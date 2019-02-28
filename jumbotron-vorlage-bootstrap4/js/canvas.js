d6f1d4335f79ae17df99f023eb1e85f89793910a
SaveEnergy <46645793+SaveEnergy@users.noreply.github.com>
2019-02-28 10:30:34 +0100

    fixed the Game :grimacing:

--- jumbotron-vorlage-bootstrap4/js/canvas.js
+++ jumbotron-vorlage-bootstrap4/js/canvas.js
@@ -14,22 +14,28 @@ sell.addEventListener("click", sellAktie);
 let kontoStand = 10000;
 let aktienStand = 0;
 let interval;
-let step = 5;
+let step = 1;
 let x = 0;
 let y = Math.floor(Math.random() * canvas.canvas.height);
 
 function startGame() {
     interval = setInterval(gameLogic, 1000);
 
-    canvas.lineJoin = "miter";
+    canvas.lineJoin = "round";
     canvas.beginPath();
     canvas.moveTo(x, y);
+}
+
+function updateVariables() {
+    konto.innerText = kontoStand;
+    aktien.innerText = aktienStand;
 }
 
 function buyAktie() {
     if (kontoStand > 0) {
         kontoStand -= canvas.canvas.height - y;
         aktienStand++;
+        updateVariables();
     }
 }
 
@@ -37,24 +43,31 @@ function sellAktie() {
     if (aktienStand > 0) {
         kontoStand += canvas.canvas.height - y;
         aktienStand--;
+        updateVariables();
     }
 }
 
 function gameLogic() {
     x += step;
-    y += Math.floor(Math.random() * canvas.canvas.height) % step ? step : -step;
-    canvas.lineTo(x , y);
+
+    if (y <= 10) {
+        y += 4;
+    }
+    if (y >= (canvas.canvas.height - 10)) {
+        y -= 4;
+    } else {
+        y += Math.floor(Math.random() * 10) % 2 ? 4 : -4;
+    }
+
+    canvas.lineTo(x, y);
     console.log(x, y);
     canvas.stroke();
 
     kurs.innerText = canvas.canvas.height - y;
     rest.innerText = (canvas.canvas.width - x) / step;
 
-    konto.innerText = kontoStand;
-    aktien.innerText = aktienStand;
 
-
-    if (x > canvas.canvas.width) {
+    if (x >= canvas.canvas.width) {
         clearInterval(interval);
     }
 }
